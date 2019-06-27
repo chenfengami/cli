62e87fab325d43287c9e96cffbf07e73fb6bbc9e
chenfengami <18094159624@163.com>
2019-06-27 23:19:21 +0800

    commit合并add操作

--- cli.js
+++ cli.js
@@ -1,13 +1,23 @@
 #!/usr/bin/env node
 
 const program = require('commander');
+/**
+ * @desc 子进程
+ * @desc .exec 衍生一个 shell 并在该 shell 中运行命令，当完成时则将 stdout 和 stderr 传给回调函数。
+ */
+const exec = require('child_process').exec;
 
 program
-  .version('0.1.0')
-  .option('-n, --yourname [yourname]', 'Your name')
-  .option('-g, --glad', 'Tell us you are happy')
+  .option('-a, --add', 'git add .')
+  .option('-m, --commit <>', 'git commit')
   .parse(process.argv);
 
-if (program.yourname) {
-  // TODO
+// TODO 需要完善
+if (program.add) {
+  // exec('git add .');
+}
+
+if (program.commit) {
+  exec('git add .');
+  exec('git commit -m' + program.commit);
 }
